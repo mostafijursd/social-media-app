ff12c1d92b41043e46db4a7ac1079a812ce462a8
Mostafijur Rahaman <148426873+mostafijursd@users.noreply.github.com>
2024-01-08 21:30:52 +0530

    change

--- frontend/src/assets/data.js
+++ frontend/src/assets/data.js
@@ -1,7 +1,7 @@
 export const user = {
     _id: "64df3c064180b81adfe41d4b",
     firstName: "Code",
-    lastName: "Wave",
+    lastName: "man",
     email: "codewavewithasante@gmail.com",
     friends: [{
             _id: "64df3aec4180b81adfe41d32",
--- frontend/src/components/PostCard.jsx
+++ frontend/src/components/PostCard.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom';
-import { NoProfile } from '../assets';
+import { NoProfile  } from '../assets';
+import { postComments } from '../assets/data';
 import moment from 'moment'
 import { BiSolidLike } from "react-icons/bi";
 import { BiLike } from "react-icons/bi";
@@ -8,6 +9,42 @@ import { BiComment } from "react-icons/bi";
 import { MdOutlineDeleteOutline } from "react-icons/md";
 import {useForm} from "react-hook-form"
 import TextInput from './TextInput'
+import{CustomButton,Loading} from '../components'
+
+
+
+  const ReplyCard=({reply,user,handleLike})=>{
+    return(
+      <div className='w-full py-3 ' >
+<div className='flex gap-3 items-center mb-1'>
+<Link to={"/profile/"+reply?.userId?._id}>
+<img src={reply?.userId?.profileUrl?? NoProfile} alt={reply?.userId?.firstName}  className='w-10 h-10 rounded-full object-cover' />
+</Link>
+
+<div>
+  <Link  to={"/profile/"+reply?.userId?._id}>
+
+    <p className='font-medium text-base text-ascent-1'>{reply?.userId.firstName}{reply?.userId.lastName}</p>
+  </Link>
+  <span className='text-ascent-2 text-sm'>{moment(reply?.createdAt?? "2024-01-08").fromNow()}</span>
+</div>
+</div>
+<div  className='ml-12'>
+ <p className='text-ascent-2'>{reply?.comment}</p>
+ <div className='mt-2 flex gap-6'>
+<p className='flex gap-2 items-center text-base text-ascent-2 cursor-pointer' onClick={handleLike}>
+{reply?.likes?.includes(user?._id)?(
+  <BiSolidLike size={20} color='blue'/>
+):(<BiLike size={20} />)
+
+}{reply?.likes?.length} Likes
+</p>
+ </div>
+</div>
+      </div>
+    )
+  }
+
 const CommentForm=({user,id ,replyAt,getComments})=>{
 
   const [loading,setLoading]=useState(false);
@@ -38,6 +75,31 @@ return(
              
              />
 </div>
+{
+  errMsg?.message &&(
+    <span role='alert' className={`text-sm ${errMsg?.status ==="failed"
+         ?"text-[#f64949fe]"
+         :"text-[#2ba150fe]"
+  }
+   mt-0.5 `}>
+      {errMsg?.message}
+    </span>
+  )}
+
+  <div  className='flex items-center justify-end pb-2'>
+{
+  loading?(
+    <Loading/>
+  ):(
+    <CustomButton 
+    title='Submit'
+    type='submit'
+    containerStyles='bg-[#0444a4] text-white py-1 px-3 rounded-full font-semibold text-sm'
+    
+    />
+  )
+}
+  </div>
 </form>
 
 )
@@ -53,7 +115,13 @@ function PostCard({post,user,deletePost,likePost}) {
     const[loading,setLoading]=useState(false);
     const[replyComments,setReplyComments]=useState(0);
     const[showComments,setShowComments]=useState(0)
-  const getComments=async()=>{}
+  const getComments=async()=>{
+
+    setReplyComments(0)
+    setComments(postComments)
+    setLoading(false)
+  };
+   const handleLike=async()=>{}
 
   return (
     <div  className='mb-2 bg-primary p-4 rounded-xl'>
@@ -132,6 +200,92 @@ function PostCard({post,user,deletePost,likePost}) {
       getComments={()=>getComments(post?._id)}
     
     />
+
+    {
+      loading?(<Loading/>
+      ): comments?.length>0 ?( comments?.map((comment)=>(
+        <div className='w-full py-2' key={comment?._id}>
+          <div className='flex gap-3 items-center mb-1'>
+
+<Link   to={"/profile/"+ comment ?.userId?._id}>
+
+  <img  src={comment?.userId?.profileUrl ?? NoProfile}
+     alt={comment?.userId?.firstName}
+     className='w-10 h-10 rounded-full object-cover' />
+</Link>  
+</div>
+<div className='ml-12'>
+<p className='text-ascent-2'>{comment?.comment}</p>
+<div className='mt-2 flex gap-6'>
+<p className='flex gap-2 items-center text-base text-ascent-2 cursor-pointer'>
+  
+  {
+    comment?.likes?.includes(user?._id)?(
+      <BiSolidLike size={20} color='blue'/>
+    ):(<BiLike size={20}/>)
+  }
+  {comment?.likes?.length} Likes
+</p>
+<span className='text-blue cursor-pointer' onClick={()=>setReplyComments(comment?._id)}>Reply</span>
+</div>
+{
+  replyComments===comment?._id &&(
+    <CommentForm 
+      user={user}
+      id={comment?._id}
+      replyAt={comment?.form}
+      getComments={()=>getComments(post?._id)}
+    />
+  )
+}
+</div>
+{/* Replies */}
+
+<div className='py-2 px-8 mt-6'>
+{comment?.replies?.length>0 && (
+  <p className='text-base text-ascent-1 cursor-pointer'
+  
+  onClick={()=>
+    setShowReply(showReply === comment?.replies?._id
+    ? 0
+    : comment?.replies?._id)
+  }
+  >Show Replies({comment?.replies?.length})</p>
+
+)}
+
+{
+
+  showReply===comment?. replies?._id &&
+   comment?.replies?.
+  map((reply)=>(
+    <ReplyCard reply={reply} user={user} key={reply?._id}
+      handleLike={()=>
+      handleLike("/post/like-comment/"+ 
+       comment?._id + "/" +
+       reply?._id
+       )
+      }
+    
+    />
+  ))
+}
+</div>
+ <div>
+
+<Link to={"/proflie/"+ comment?.userId?._id}>
+
+<p className='font-medium text-base text-ascent-1'>{comment?.userId?.firstName}{comment?.userId?.lastName}</p>
+
+</Link>
+<span  className='text-ascent-2 text-sm'>{moment(comment?.createdAt ?? "2024-01-08").fromNow()}</span>
+ </div>
+
+        </div>
+      ))
+       
+      ):(<span className='flex text-sm py-4 text-ascent-2 text-center'>No Comments, be first to comment</span>)
+    }
    </div>
 )}
     </div>
